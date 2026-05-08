07539855fc4f89f2cc4f9ce194ea6797853953ef
j0z <j_alberto_v1@hotmail.com>
2026-05-08 14:24:35 -0600

    entry poin sh added

---
+++ docker/rapidpro/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+export REMOTE_CONTAINERS=true
+export POSTGIS=off
+
+ACTION=${1:-webapp}
+
+if [ "$ACTION" = "webapp" ]; then
+    echo "Running RapidPro webapp..."
+
+    poetry run python manage.py migrate
+    poetry run python manage.py migrate_dynamo
+
+    exec poetry run gunicorn temba.wsgi:application \
+        --bind 0.0.0.0:8000 \
+        --workers 12 \
+        --threads 2 \
+        --timeout 60
+
+elif [ "$ACTION" = "celery" ]; then
+    echo "Running RapidPro celery worker..."
+
+    exec poetry run celery -A temba worker -E -B --loglevel=INFO
+
+else
+    echo "Unknown action: $ACTION"
+
+    exit 1
+fi
